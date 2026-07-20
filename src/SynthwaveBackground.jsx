e773a5b9bd4bd55f963e48b34693a8990fa3cc0d
soham-0207 <silentdevil1801@gmail.com>
2026-07-21 01:44:48 +0530

    Implement Retro Synthwave Grid background theme

---
+++ src/SynthwaveBackground.jsx
@@ -0,0 +1,11 @@
+import './SynthwaveBackground.css';
+
+export default function SynthwaveBackground() {
+  return (
+    <div className="synthwave-container">
+      <div className="synthwave-sun"></div>
+      <div className="synthwave-horizon"></div>
+      <div className="synthwave-grid"></div>
+    </div>
+  );
+}
